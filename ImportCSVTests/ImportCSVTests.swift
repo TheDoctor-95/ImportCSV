bd1eadbd15691129df760167614fb4108c932121
dmorenoar <dmorenoar@192.168.170.128>
2018-12-29 09:19:02 -0800

    Initial Commit

---
+++ ImportCSVTests/ImportCSVTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImportCSVTests.swift
+//  ImportCSVTests
+//
+//  Created by dmorenoar on 29/12/18.
+//  Copyright © 2018 dmorenoar. All rights reserved.
+//
+
+import XCTest
+@testable import ImportCSV
+
+class ImportCSVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
